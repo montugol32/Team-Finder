deab8b1d984b8d7487af68fb3737ad999219f586
saurabh <guptasaurabh5858@gmail.com>
2022-07-03 16:13:38 +0530

    login and signup

--- controllers/user.js
+++ controllers/user.js
@@ -3,17 +3,41 @@ const {validationResult} = require('express-validator')
 const user = require("../models/user")
 var jwt = require('jsonwebtoken')
 var expressJwt = require('express-jwt')
+const multer= require('multer')
+
+const { diskStorage } = require("multer")
+
+const storage=multer.diskStorage({
+
+  destination:function(request,file,callback){
+
+    callback(null,'./public/upload/images')
+  },
+  filename:function (request,file,callback){
+    callback(null,Date.now()+file.originalname)
+
+  }
+
+});
+
+const upload= multer({
+
+  storage: storage,
+  limits:{
+    fileSize: 1024*1021*3,
+  },
+});
 
 
-exports.signup = (req, res) => {
+exports.signup = upload.single('image'),async(req, res) => {
+    
   const errors = validationResult(req)
-
   if(!errors.isEmpty()) {
     return res.status(400).json({
       error: errors.array()[0].msg
     })
-  }
-
+    }
+  console.log(req.body);
   const user = new User(req.body)
   user.save((err, user) => {
     if(err) {
@@ -22,10 +46,12 @@ exports.signup = (req, res) => {
       })
     }
 
-    return res.json({
-      message: "Success",
-      user
-    })
+    // return res.json({
+    //   message: "Success",
+    //   user
+    // })
+    
+    res.redirect('/api/home')
   })
 }
 
@@ -55,14 +81,17 @@ exports.signin = (req, res) => {
 
     // Send response
     const {_id, name, email} = user
-    return res.json({
-      token,
-      user: {
-        _id,
-        name,
-        email
-      }
-    })
+    
+    // return res.json({
+    //   token,
+    //   user: {
+    //     _id,
+    //     name,
+    //     email
+    //   }
+    // })
+    
+    res.redirect('/api/home')
     
   })
 }
--- routes/user.js
+++ routes/user.js
@@ -2,13 +2,31 @@ const express = require("express")
 const { signup, signin, signout } = require("../controllers/user")
 const {check} = require('express-validator')
 const router = express.Router()
+const Users=require('./../models/user')
+
+router.get('/home',async(req,res)=>{
+  const users=await Users.find();
+  console.log(users);
+  res.render('api/home',{users: users});
+})
+
+router.get('/signup',(req,res)=>{
+  res.render('api/signup',{});
+})
+
 
 router.post('/signup', [
   check("name", "Name atleast should be 3 characters").isLength({min: 3}),
   check("email", "Email should be valid").isEmail(),
   check("password", "Password at least should be 6 characters").isLength({min: 6}),
+  check("college_name", "college_name required").isLength({min: 4}),
+  check("interest", "Interest are required").isLength({min: 6}),
+  
 ] ,signup)
 
+router.get('/signin',(req,res)=>{
+  res.render('api/signin');
+})
 router.post('/signin', signin)
 
 router.get("/signout", signout)
